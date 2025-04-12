14d1056eed071d2e09bf17b505ec713095094951
Nuno Ferreira <nuno.ferreira@cambridgespark.com>
2025-04-12 17:45:08 +0100

    Get the DF from data pulled by API

--- get_data.py
+++ get_data.py
@@ -1,7 +1,13 @@
 from urllib.request import urlopen
 import json
+import pandas as pd
 
 # Example of how to get data from the API
 response = urlopen('https://api.openf1.org/v1/car_data?driver_number=55&session_key=9159&speed>=315')
 data = json.loads(response.read().decode('utf-8'))
-print(data)
+
+# Convert the data to a pandas DataFrame
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print(df)
